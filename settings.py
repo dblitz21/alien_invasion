7efe8ad655effccc1c515a8eb882b0d03b643546
dblitz <blitzy2k@gmail.com>
2017-08-15 23:45:33 -0400

    added alien fleet to the game

--- settings.py
+++ settings.py
@@ -4,8 +4,8 @@ class Settings():
 	def __init__(self):
 		"""Initialize the game's settings."""
 		# Screen settings
-		self.screen_width = 600
-		self.screen_height = 400
+		self.screen_width = 800
+		self.screen_height = 600
 		self.bg_color = (230, 230, 230)
 		
 		# Ship Settings
@@ -17,3 +17,9 @@ class Settings():
 		self.bullet_height = 15
 		self.bullet_color = 60, 60, 60
 		self.bullets_allowed = 3
+		
+		# Alien settings
+		self.alien_speed_factor = 1
+		self.fleet_drop_speed = 10
+		# fleet_direction of 1 represents right; -1 represents left.
+		self.fleet_direction = 1
