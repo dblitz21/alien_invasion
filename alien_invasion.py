e76fe21b4f6125f82dc3e9b609f1586de8b5d960
dblitz <blitzy2k@gmail.com>
2017-08-24 00:16:26 -0400

    Added scoring, displays and high scores to the game

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ from pygame.sprite import Group
 
 from settings import Settings
 from game_stats import GameStats
+from scoreboard import Scoreboard
 from button import Button
 from ship import Ship
 from alien import Alien
@@ -20,7 +21,7 @@ def run_game():
 	play_button = Button(ai_settings, screen, "Play")
 	# Create an instance to store game statistics.
 	stats = GameStats(ai_settings)
-	
+	sb = Scoreboard(ai_settings, screen, stats)
 	# Make a Ship, a group of bullets and a group of aliens 
 	ship = Ship(ai_settings, screen)
 	bullets = Group()
@@ -35,15 +36,17 @@ def run_game():
 	
 	# Start the main loop for the game.
 	while True:
-		gf.check_events(ai_settings, screen, stats, play_button, ship, 
+		gf.check_events(ai_settings, screen, stats, sb, play_button, ship, 
 			aliens, bullets)
 		
 		if stats.game_active:
 			ship.update()
-			gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
-			gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+			gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, 
+				bullets)
+			gf.update_aliens(ai_settings, screen, stats, sb, ship, aliens, 
+				bullets)
 		
-		gf.update_screen(ai_settings, screen, stats,ship, aliens, bullets, 
+		gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, 
 			play_button)
 	
 run_game()
